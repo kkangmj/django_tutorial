09f7cbb0f7e4004d22db478648b1e39377770452
kkangmj <bbak0402dd@gmail.com>
2020-03-02 23:29:38 +0900

    modified:   polls/__pycache__/urls.cpython-36.pyc 	modified:   polls/__pycache__/views.cpython-36.pyc 	modified:   polls/urls.py 	modified:   polls/views.py

--- polls/urls.py
+++ polls/urls.py
@@ -5,12 +5,6 @@ from . import views
 
 app_name = 'polls'
 urlpatterns=[
-    '''
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name="detail"),
-    path('<int:question_id>/result/', views.result, name="result"),
-    path('<int:question_id>/vote/', views.vote, name="vote"),
-    '''
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/result/', views.ResultsView.as_view(), name='result'),
